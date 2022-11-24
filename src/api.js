d8e68be2111973cfbbd4030544744044cad2eba6
thomasr2240 <thomasr2240@gmail.com>
2022-11-24 11:26:48 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ const api = {
   rootUrl: '/',
   // baseUrl: 'http://95.216.85.81:5000/api', //mock data base folder
   // imgUrl: "http://95.216.85.81:5000/uploads/",
-  baseUrl: 'https://blackhole-backend.herokuapp.com/api',
-  imgUrl: 'https://blackhole-backend.herokuapp.com/uploads/',
+  baseUrl: 'https://blackholebackend.vercel.app:5006/api',
+  imgUrl: 'https://blackholebackend.vercel.app:5006/uploads/',
   user: '/users',
   collection: '/collections',
   utils: '/utils'
